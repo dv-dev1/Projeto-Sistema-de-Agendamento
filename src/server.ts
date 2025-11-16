91250be79fc362a90e7b1bd4ed90b1d642cb4b53
Jose Gabriel <josegabriel9004@gmail.com>
2025-11-16 15:00:46 -0300

    (server): Corrige inicialização do servidor e dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,30 @@
-import express from "express";
-import "dotenv/config";
-import { connectDB } from "./config/db";
+// server.ts
 
+// 1. CARREGAMENTO DOTENV 
+import 'dotenv/config'; 
+
+// 2. IMPORTS ESSENCIAIS
+import { connectDB } from "./config/db";
+import express from 'express';
 import cors from "cors";
 
+// 3. INICIALIZAÇÃO
 const app = express();
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
+    cors({
+        origin: "http://localhost:5173",
+    })
 );
 
 app.use(express.json());
 
-connectDB();
+// 4. CONEXÃO COM O BANCO DE DADOS 
+connectDB(); 
 
+// 5. ROTAS E MÉTODOS 
 app.get("/", (req, res) => {
-  res.send("API de Agendamento está no ar!");
+    res.send("API de Agendamento está no ar!");
 });
 
 import authRoutes from "./routes/auth.routes";
@@ -30,14 +37,15 @@ import professionalRoutes from "./routes/professional.routes";
 app.use("/api/professionals", professionalRoutes);
 
 import appointmentRoutes from "./routes/appointment.routes";
-
 app.use("/api/appointments", appointmentRoutes);
 
 import availabilityRoutes from "./routes/availability.routes";
 app.use("/api/availability", availabilityRoutes);
 
+
+// 6. INÍCIO DO SERVIDOR
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
